3112bb121803994bfc8ca60786fa3616aa592edb
th3san4ar <113807460+th3san4ar@users.noreply.github.com>
2022-10-11 17:30:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+STUDENT_ID = 11
+
+
+@app.route(f'/api/v1/hello-world-{STUDENT_ID}')
+def hello_world():
+    return f"Hello world {STUDENT_ID}", 200
+
+
+if __name__ == "__main__":
+    print("succesfull")
+    serve(app, port=5000)
